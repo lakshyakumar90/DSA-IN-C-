668504be24e9f72fee443bcb2d1a04f19a547f12
Lakshya Kumar <lakshyakumar5023@gmail.com>
2024-10-26 09:08:35 +0530

    time complexity basics

---
+++ basics.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x;
+    cin>>x;
+    cout<< "Hey" << x ;
+
+    //Data Types
+    // int, long, long long, float, double, long double
+    // string getline
+    // char 
+
+    string str;
+    cin >> str; //only takes string before a whitespace 
+    getline(cin, str); //takes string after a whitespace
+
+
+    return 0;
+}
